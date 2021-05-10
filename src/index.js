df0db20ffd2adae5cce3a4e1938b333609a718bc
Ambre Chognot <a.chognot64@gmail.com>
2021-05-10 08:50:20 +0200

    Ajout corrections

--- src/index.js
+++ src/index.js
@@ -1,153 +1,71 @@
-// Affichage d'un tableau
-// Init du tableau
-const plate = [
-  ['.','.','.'],
-  ['.','.','.'],
-  ['.','.','.'],
-];
+const EMPTY_BOX = '.';
 
-// Tableau joueurs
-const players = [
-  { name: 'player1', sign: 'x' },
-  { name: 'player2', sign: 'o' },
-];
+const X_SIGN = 'x';
+const O_SIGN = 'o';
 
-let joueurCourant = "X";
-const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+const PLATE_X_SIZE = 3;
+const PLATE_Y_SIZE = 3;
 
-// Affichage du tableau
+const PLATE_SIZE = PLATE_X_SIZE * PLATE_Y_SIZE;
+
+function isCoordValid(c) {
+  return c >= 0 && c <= 2;
+}
+
+function getBoxIdFromXY(x, y) {
+  return (x * PLATE_X_SIZE) + y;
+}
+
+function getBox(plate, x, y) {
+  return plate[getBoxIdFromXY(x, y)];
+}
+
+function setBox(plate, x, y, sign) {
+  const newPlate = plate;
+  newPlate[getBoxIdFromXY(x, y)] = sign;
+  return newPlate;
+}
+
 function printPlate(plate) {
-  plate.forEach((line) => console.log(line));
+  plate.forEach((box, i) => (((i + 1) % PLATE_X_SIZE)
+    ? process.stdout.write(` ${box} `)
+    : console.log(` ${box}\n`)));
 }
 
-// Init questions
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+function initPlate() {
+  return [...Array(PLATE_SIZE).keys()].map(() => EMPTY_BOX);
+}
 
-// Choix cases
-printPlate(plate) // Affiche tableau avec case remplie
-console.log(`Au tour de joueur1 !`);
-rl.question("Choisis la coordonée x : ", function(NumberX) {
-    if (NumberX<0 || NumberX>2) {
-      throw new Error('Le nombre doit être entre 0 et 2');
-    }
-    rl.question("Choisis la coordonée y : ", function(NumberY) {
-        if (NumberY<0 || NumberY>2) {
-          throw new Error('Le nombre doit être entre 0 et 2');
-        }
-        console.log(`Tu as joué en ${NumberX} : ${NumberY} !`);
-        plate[NumberY][NumberX] = 'x'; // Case remplie
-        printPlate(plate) // Affiche tableau avec case remplie
-        rl.close();
-    });
-});
+function isCaseEmpty(plate, x, y) {
+  return getBox(plate, x, y) === EMPTY_BOX;
+}
 
-// Vérifications si le tableau est vide
-function isEmpty(plate, x, y) {
-   if (plate[x][y] != 0) {
-     throw new Error('Case is not empty');
-   }   
+function setSign(plate, x, y, sign) {
+  return isCoordValid(x)
+    && isCoordValid(y)
+    && isCaseEmpty(plate, x, y)
+    && setBox(plate, x, y, sign);
+}
+
+function isThereAWinner() {
+  console.log('Implement it for next monday based on the other function');
+}
+
+function isGameNull(plate) {
+  return !isThereAWinner(plate) && !plate.reduce((state, box) => box === EMPTY_BOX || state, false);
+}
+
+export {
+  isCaseEmpty,
+  isCoordValid,
+  getBoxIdFromXY,
+  getBox,
+  setBox,
+  printPlate,
+  initPlate,
+  setSign,
+  isGameNull,
+  isThereAWinner,
+  X_SIGN,
+  O_SIGN,
 };
-
-function isAlign(plate, x, y) {
-  // possible horizontal alignments
-  if(plate[0][0] == 'x' && plate[1][0] == 'x' && plate[2][0] == 'x'){
-    console.log(`Joueur MACHIN a gagné !`);
-    rl.close();
-  }
-  checkSet.clear();
-  if(plate[0][1] && plate[1][1] && plate[2][1]){
-    console.log(`Joueur MACHIN a gagné !`);
-    rl.close();
-  }
-  checkSet.clear();
-  if(plate[0][2] && plate[1][2] && plate[2][2]){
-    console.log(`Joueur MACHIN a gagné !`);
-    rl.close();
-  }
-  checkSet.clear();
-  // possible vertical alignments
-  if(plate[0][0] && plate[0][1] && plate[0][2]){
-    console.log(`Joueur MACHIN a gagné !`);
-    rl.close();
-  }
-  checkSet.clear();
-  if(plate[1][0] && plate[1][1] && plate[2][2]){
-    console.log(`Joueur MACHIN a gagné !`);
-    rl.close();
-  }
-  checkSet.clear();
-  if(plate[2][0] && plate[2][1] && plate[2][2]){
-    console.log(`Joueur MACHIN a gagné !`);
-    rl.close();
-  }
-  checkSet.clear();
-  // possible diagonal alignments
-  if(plate[0][0] && plate[1][1] && plate[2][2]){
-    console.log(`Joueur MACHIN a gagné !`);
-    rl.close();
-  }
-  checkSet.clear();
-  if(plate[0][2] && plate[1][1] && plate[2][0]){
-    console.log(`Joueur MACHIN a gagné !`);
-    rl.close();
-  }
-  checkSet.clear();
-}
-
-// Nettoyage tableau
-function clearBox(plate) {
-  document.getElementById(plate).innerText="";
-}
-
-function handlePlayerChange() {
-  currentPlayer = currentPlayer === "X" ? "O" : "X";
-  statusDisplay.innerHTML = currentPlayerTurn();
-}
-
-// ____________________________
-
-// playAction(plate, x, y, sign) => plate
-
-// commencer Partie
-// while tableau pas rempli or pas alignement 3 cases{
-// afficher tableau pour joueur 1
-//   tour du joueur 1
-//   remplie tableau
-//   if (case pas 0){ // fonction isEmpty
-//     redemande case à remplir
-//   }
-//   else {
-//     remplacer dans la case par le signe du joueur
-//     sauvegarde tableau
-//   }
-// afficher tableau pour joueur 2
-//   tour du joueur 2
-//   remplie tableau
-//   if (case pas 0){ // fonction isEmpty
-//     redemande case à remplir
-//   }
-//   else {
-//     remplacer dans la case par le signe du joueur
-//     sauvegarde tableau
-//   }
-// }
-
-// Exemple :
-// function playAction(plate, x, y, sign) {
-//   checkCoord(x, y);
-//   checkSign(sign);
-//   isEmpty(plate, x, y); si pas libre, throw et renvoi la demande de remplissage de case
-//   plate[x][y] = sign;
-
-// return plate;
-// }
-
-// function isEmpty(plate, x, y) {
-//   if (plate[x][y] != 0) {
-//     throw new Error('Case is not empty');
-//   }   
-// }
